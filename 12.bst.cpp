fa1c1ebb89386f5897682cd12f05250e888d279d
Hastej Singh <hastejsin@gmail.com>
2020-10-15 19:03:15 +0530

    12

---
+++ 12.bst.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left=nullptr;
+    Node* right=nullptr;
+
+    Node(int data){
+        this->data=data;
+    }
+};
+
+void display(Node* node){
+    if(node==nullptr) return;
+
+    string str="";
+    str+=(node->left!=nullptr ? to_string(node->left->data):"-");
+    str+=" <- "+to_string(node->data)+" -> ";
+    str+=(node->right!=nullptr ? to_string(node->right->data):"-");
+    cout<<str<<endl;
+
+    display(node->left);
+    display(node->right);
+}
+
+bool search(Node* node, int data){
+    
+    Node* curr=node;
+    while (curr!=nullptr)
+    {
+        if(curr->data==data) return true;
+        else if(curr->data < data)curr= curr->right;
+        else
+        curr=curr->left;
+    }
+    return false;
+    
+}
+
+Node* addData(Node* node, int data){
+    if(node==nullptr) return new Node(data);
+    if(node->data >data)node->left= addData(node->left,data);
+
+    if(node->data<data) node->right= addData(node->right,data);
+
+    return node;
+}
+
+int main()
+{
+    cout<<"Enter first node : ";
+    int valroot;
+    cin>>valroot;
+
+    Node* node = new Node(valroot);
+
+    display(node);
+
+    int choice;
+
+    int n = 0;
+
+    while (n!=4)
+    {
+        cout << "INTPUT NUMBER : "<<endl;
+        
+        cout << "1. ADD "<<endl;
+        cout << "2. SEARCH"<<endl;
+        cout << "3. DISPLAY"<<endl;
+        cout << "4. EXIT"<<endl;
+        cin >> n;
+
+        cout<<endl;
+
+        if(n==1){
+            int val;
+            cout << "Enter the number you want to add: ";
+            cin >> val;
+            addData(node,val);
+        }
+         else if(n==2){
+            int num;
+            cout<<"Enter the number : ";
+            cin>>num;
+            if(search(node,num)==true)
+            {
+                cout<<num<<" is  present in the tree"<<endl;
+            }else
+            {
+                cout<<num<<" is not present in the tree"<<endl;
+            }
+            
+            break;
+        }
+        else if(n==3)
+            display(node);
+        else break;
+    }
+
+    return 0;
+}
