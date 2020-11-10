1008c9556045a92582440d4f46ca0be00eb9e735
Hastej Singh <hastejsin@gmail.com>
2020-11-10 17:11:42 +0530

    14th prog

---
+++ 14.insort.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+
+using namespace std; 
+
+void insertionsort(vector<int> &arr, int n) 
+{ 
+	int key, j; 
+	for (int i = 1; i < n; i++) 
+	{ 
+		key = arr[i]; 
+		j = i - 1; 
+
+		while (j >= 0 && arr[j] > key) 
+		{ 
+			arr[j + 1] = arr[j]; 
+			j = j - 1; 
+		} 
+
+		arr[j + 1] = key; 
+	} 
+} 
+
+int main() 
+{ 
+    int n;
+    cout<<"Enter size of array : ";
+    cin>>n;
+
+	vector<int> arr(n,0); 
+
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    cout<<endl<<"Array is : ";
+
+    for (int i = 0; i < n; i++) 
+    {
+		cout << arr[i] << " "; 
+    }
+
+    cout<<endl<<endl;
+
+
+	insertionsort(arr, n); 
+
+    cout<<"Sorted array is : ";
+
+    for (int i = 0; i < n; i++) 
+    {
+		cout << arr[i] << " "; 
+    }
+
+	cout << endl;
+
+	return 0; 
+}
